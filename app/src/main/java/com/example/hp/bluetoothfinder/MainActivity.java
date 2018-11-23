2a3043aa7b1fd93e942b37156f7f6d4ab2ec09ba
areeshaFatima <areeshaf7@gmail.com>
2018-11-24 01:03:08 +0530

    bluetooth device searched

--- app/src/main/java/com/example/hp/bluetoothfinder/MainActivity.java
+++ app/src/main/java/com/example/hp/bluetoothfinder/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.hp.bluetoothfinder;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -12,10 +19,24 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     TextView textView;
     Button button;
+    BluetoothAdapter bluetoothAdapter;
+
+    private final BroadcastReceiver broadcastReceiver=new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action=intent.getAction();
+            Log.i("ACTION",action);
+            if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                textView.setText("Finished");
+                button.setEnabled(true);
+            }
+        }
+    };
 
     public void searchFunc(View view){
         textView.setText("Searching...");
         button.setEnabled(false);
+        bluetoothAdapter.startDiscovery();
     }
 
     @Override
@@ -26,5 +47,15 @@ public class MainActivity extends AppCompatActivity {
         textView=findViewById(R.id.textView);
         listView=findViewById(R.id.listView);
         button=findViewById(R.id.button);
+
+        bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        IntentFilter intentFilter=new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(broadcastReceiver,intentFilter);
+
+
     }
 }
